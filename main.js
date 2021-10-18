cae1c4a232e3cb1de29170e959eb82a5c7bf4062
Ivan Andrade <ivan.andrade.prado@gmail.com>
2021-10-17 21:17:31 -0500

    Intro[Saludo]

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 const suma = (num1, num2) => console.log(num1 + num2);
 
 suma(3,4);
+
+function saludo(persona){
+    const nombre = persona; 
+    console.log('Hola, ', nombre);
+}
+
+saludo('Ivan');
